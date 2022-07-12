f028ed505348db90f9077dc6b01d980d58872392
Katerin Calderon <kccc0713@gmail.com>
2022-07-11 19:36:34 -0500

    add class player and card and export as modules

---
+++ src/card.js
@@ -0,0 +1,13 @@
+class Card{
+    constructor({
+        name,
+        suit,
+        value
+    }){
+        this.name = name;
+        this.suit = suit;
+        this.value = value
+    }
+}
+
+module.exports = Card;
---
+++ src/player.js
@@ -0,0 +1,11 @@
+class Player{
+    constructor({
+        id,
+        name
+    }){
+        this.id = id;
+        this.name = name
+    }
+}
+
+module.exports = Player;
